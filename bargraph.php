1501287f1b49ee341b6b65386d5dc37bec3579ec
Shivam Bansal <bshivam4@gmail.com>
2017-07-27 22:33:00 +0530

    Initialized bargraph.php

---
+++ bargraph.php
@@ -0,0 +1,15 @@
+<?php
+include_once 'header.php';
+?>
+<body>
+    <nav class="teal lighten-3">
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo center">Feedback Report (Bargraph)</a>
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+					<li><a href="logout.php" class="right"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a></li>
+				</ul>
+      </div>
+    </nav>
+		<div class="container">
+        </div>
+</body>
