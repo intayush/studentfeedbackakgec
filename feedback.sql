32c87a73ae39ba0663189aec732f211b7deb5618
Ayush Singh <ayush.singh.96@gmail.com>
2017-08-11 13:59:19 +0530

    new sql added

--- feedback.sql
+++ feedback.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 19, 2017 at 11:43 PM
+-- Generation Time: Aug 11, 2017 at 01:57 PM
 -- Server version: 5.7.18-0ubuntu0.16.04.1
 -- PHP Version: 7.0.15-0ubuntu0.16.04.4
 
@@ -23,6 +23,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('admin', '9948b2587fe40b2ffd6faec84b1d7abe');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `faculty`
 --
 
@@ -38,8 +56,28 @@ CREATE TABLE `faculty` (
 
 INSERT INTO `faculty` (`id`, `name`, `department`) VALUES
 (1, 'ayush', 'cse'),
-(2, 'shivang', 'me'),
-(3, 'null', 'null');
+(2, 'shivam', 'me'),
+(3, 'shubham', 'ece');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lab_assistant`
+--
+
+CREATE TABLE `lab_assistant` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `department` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lab_assistant`
+--
+
+INSERT INTO `lab_assistant` (`id`, `name`, `department`) VALUES
+(1, 'ritik', 'cse'),
+(2, 'jagmohan', 'me');
 
 -- --------------------------------------------------------
 
@@ -66,10 +104,35 @@ CREATE TABLE `practical_faculty_response` (
 --
 
 INSERT INTO `practical_faculty_response` (`id`, `student_id`, `faculty_id`, `subject`, `year`, `section`, `q1`, `q2`, `q3`, `q4`, `q5`) VALUES
-(1, 1, 1, 'ncs551', 4, 'cs1', 4, 5, 5, 3, 3),
-(2, 1, 2, 'ncs551', 4, 'cs1', 4, 5, 4, 2, 2),
-(3, 1, 2, 'ncs552', 4, 'cs1', 4, 4, 3, 2, 2),
-(4, 1, 1, 'ncs552', 4, 'cs1', 3, 3, 2, 2, 1);
+(1, 3, 1, 'NCS-751', 4, 'CS-1', 4, 2, 4, 2, 5),
+(2, 3, 2, 'NCS-751', 4, 'CS-1', 2, 2, 3, 5, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `practical_lab_assistant_response`
+--
+
+CREATE TABLE `practical_lab_assistant_response` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `la_id` int(11) NOT NULL,
+  `subject` varchar(20) NOT NULL,
+  `year` int(11) NOT NULL,
+  `section` varchar(10) NOT NULL,
+  `q1` int(11) NOT NULL,
+  `q2` int(11) NOT NULL,
+  `q3` int(11) NOT NULL,
+  `q4` int(11) NOT NULL,
+  `q5` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `practical_lab_assistant_response`
+--
+
+INSERT INTO `practical_lab_assistant_response` (`id`, `student_id`, `la_id`, `subject`, `year`, `section`, `q1`, `q2`, `q3`, `q4`, `q5`) VALUES
+(1, 3, 1, 'NCS-751', 4, 'CS-1', 3, 3, 2, 5, 5);
 
 -- --------------------------------------------------------
 
@@ -113,8 +176,8 @@ CREATE TABLE `student` (
   `year` int(11) NOT NULL,
   `section` varchar(10) NOT NULL,
   `semester` int(11) NOT NULL,
-  `tid` int(11) NOT NULL,
-  `pid` int(11) NOT NULL
+  `tid` int(11) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -122,8 +185,8 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`id`, `rollno`, `studentno`, `name`, `year`, `section`, `semester`, `tid`, `pid`) VALUES
-(1, '1402710046', '1410134', 'aysuh', 4, 'cs1', 7, 1, 1),
-(2, '33434', '44', 'hfghjfg', 2, 'cd3', 3, 2, 2);
+(3, '1402710046', '1410134', 'Ayush Singh', 4, 'CS-1', 7, 1, 1),
+(4, '1402710070', '1410152', 'Anubhav', 4, 'CS-2', 7, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -134,27 +197,31 @@ INSERT INTO `student` (`id`, `rollno`, `studentno`, `name`, `year`, `section`, `
 CREATE TABLE `student_practical` (
   `id` int(11) NOT NULL,
   `student_id` int(11) NOT NULL,
-  `f11` int(16) NOT NULL,
-  `f12` int(11) NOT NULL,
-  `l1` varchar(11) NOT NULL,
-  `f21` int(12) NOT NULL,
-  `f22` int(11) NOT NULL,
-  `l2` varchar(11) NOT NULL,
-  `f31` int(16) NOT NULL,
-  `f32` int(11) NOT NULL,
-  `l3` varchar(11) NOT NULL,
-  `f41` int(12) NOT NULL,
-  `f42` int(11) NOT NULL,
-  `l4` varchar(11) NOT NULL
+  `f11` int(16) DEFAULT NULL,
+  `f12` int(11) DEFAULT NULL,
+  `la1` int(11) DEFAULT NULL,
+  `l1` varchar(11) DEFAULT NULL,
+  `f21` int(12) DEFAULT NULL,
+  `f22` int(11) DEFAULT NULL,
+  `la2` int(11) DEFAULT NULL,
+  `l2` varchar(11) DEFAULT NULL,
+  `f31` int(16) DEFAULT NULL,
+  `f32` int(11) DEFAULT NULL,
+  `la3` int(11) DEFAULT NULL,
+  `l3` varchar(11) DEFAULT NULL,
+  `f41` int(12) DEFAULT NULL,
+  `f42` int(11) DEFAULT NULL,
+  `la4` int(11) DEFAULT NULL,
+  `l4` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `student_practical`
 --
 
-INSERT INTO `student_practical` (`id`, `student_id`, `f11`, `f12`, `l1`, `f21`, `f22`, `l2`, `f31`, `f32`, `l3`, `f41`, `f42`, `l4`) VALUES
-(1, 1, 1, 2, 'ncs551', 2, 1, 'ncs552', 3, 3, '', 3, 3, ''),
-(2, 2, 1, 2, '', 3, 3, '', 3, 3, '', 3, 3, '');
+INSERT INTO `student_practical` (`id`, `student_id`, `f11`, `f12`, `la1`, `l1`, `f21`, `f22`, `la2`, `l2`, `f31`, `f32`, `la3`, `l3`, `f41`, `f42`, `la4`, `l4`) VALUES
+(1, 1, 1, 2, 1, 'NCS-751', 2, 3, 1, 'NCS-752', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 2, 2, 1, 2, 'NCS-751', 3, 1, 1, 'NCS-752', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -166,19 +233,19 @@ CREATE TABLE `student_theory` (
   `id` int(11) NOT NULL,
   `student_id` int(11) NOT NULL,
   `t1` varchar(10) NOT NULL,
-  `f1` int(11) NOT NULL,
+  `f1` int(11) DEFAULT NULL,
   `t2` varchar(10) NOT NULL,
-  `f2` int(11) NOT NULL,
+  `f2` int(11) DEFAULT NULL,
   `t3` varchar(11) NOT NULL,
-  `f3` int(11) NOT NULL,
+  `f3` int(11) DEFAULT NULL,
   `t4` varchar(11) NOT NULL,
-  `f4` int(11) NOT NULL,
+  `f4` int(11) DEFAULT NULL,
   `t5` varchar(11) NOT NULL,
-  `f5` int(11) NOT NULL,
+  `f5` int(11) DEFAULT NULL,
   `t6` varchar(11) NOT NULL,
-  `f6` int(11) NOT NULL,
+  `f6` int(11) DEFAULT NULL,
   `t7` varchar(11) NOT NULL,
-  `f7` int(11) NOT NULL
+  `f7` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -186,8 +253,8 @@ CREATE TABLE `student_theory` (
 --
 
 INSERT INTO `student_theory` (`id`, `student_id`, `t1`, `f1`, `t2`, `f2`, `t3`, `f3`, `t4`, `f4`, `t5`, `f5`, `t6`, `f6`, `t7`, `f7`) VALUES
-(1, 1, 'ncs502', 1, 'ncs501', 2, 'ncs503', 2, '', 3, '', 3, '', 3, '', 3),
-(2, 2, 'ncs303', 1, 'ncs409', 2, '', 3, '', 3, '', 3, '', 3, '', 3);
+(1, 1, 'NCS-701', 1, 'NCS-702', 2, 'NCS-703', 1, '', NULL, '', NULL, '', NULL, '', NULL),
+(2, 2, 'NCS-701', 2, 'NCS-702', 3, 'NCS-703', 1, '', NULL, '', NULL, '', NULL, '', NULL);
 
 -- --------------------------------------------------------
 
@@ -247,8 +314,7 @@ CREATE TABLE `theory_response` (
 --
 
 INSERT INTO `theory_response` (`id`, `student_id`, `faculty_id`, `subject`, `year`, `section`, `q1`, `q2`, `q3`, `q4`, `q5`, `q6`, `q7`, `q8`, `q9`, `q10`) VALUES
-(1, 1, 1, 'ncs502', 4, 'cs1', 1, 2, 3, 4, 5, 1, 0, 1, 1, 1),
-(2, 1, 2, 'ncs503', 4, 'cs1', 1, 2, 3, 4, 5, 1, 0, 1, 1, 1);
+(1, 3, 1, 'NCS-701', 4, 'CS-1', 4, 5, 4, 3, 3, 1, 0, 1, 0, 1);
 
 --
 -- Indexes for dumped tables
@@ -261,12 +327,26 @@ ALTER TABLE `faculty`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `lab_assistant`
+--
+ALTER TABLE `lab_assistant`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `practical_faculty_response`
 --
 ALTER TABLE `practical_faculty_response`
   ADD PRIMARY KEY (`id`),
   ADD KEY `student_id` (`student_id`),
   ADD KEY `faculty_id` (`faculty_id`);
+
+--
+-- Indexes for table `practical_lab_assistant_response`
+--
+ALTER TABLE `practical_lab_assistant_response`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `faculty_id` (`la_id`);
 
 --
 -- Indexes for table `practical_questions`
@@ -294,7 +374,11 @@ ALTER TABLE `student_practical`
   ADD KEY `f31` (`f31`),
   ADD KEY `f32` (`f32`),
   ADD KEY `f41` (`f41`),
-  ADD KEY `f42` (`f42`);
+  ADD KEY `f42` (`f42`),
+  ADD KEY `la1` (`la1`),
+  ADD KEY `la2` (`la2`),
+  ADD KEY `la3` (`la3`),
+  ADD KEY `la4` (`la4`);
 
 --
 -- Indexes for table `student_theory`
@@ -333,10 +417,20 @@ ALTER TABLE `theory_response`
 ALTER TABLE `faculty`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
+-- AUTO_INCREMENT for table `lab_assistant`
+--
+ALTER TABLE `lab_assistant`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `practical_faculty_response`
 --
 ALTER TABLE `practical_faculty_response`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `practical_lab_assistant_response`
+--
+ALTER TABLE `practical_lab_assistant_response`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `practical_questions`
 --
@@ -346,7 +440,7 @@ ALTER TABLE `practical_questions`
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `theory_questions`
 --
@@ -356,7 +450,7 @@ ALTER TABLE `theory_questions`
 -- AUTO_INCREMENT for table `theory_response`
 --
 ALTER TABLE `theory_response`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
@@ -369,24 +463,31 @@ ALTER TABLE `practical_faculty_response`
   ADD CONSTRAINT `fk_student_faculty_lab_response` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `practical_lab_assistant_response`
+--
+ALTER TABLE `practical_lab_assistant_response`
+  ADD CONSTRAINT `fk_la_la` FOREIGN KEY (`la_id`) REFERENCES `lab_assistant` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_la_student` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `student`
 --
 ALTER TABLE `student`
-  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`tid`) REFERENCES `student_theory` (`id`),
-  ADD CONSTRAINT `student_ibfk_2` FOREIGN KEY (`pid`) REFERENCES `student_practical` (`id`);
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`tid`) REFERENCES `student_theory` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `student_ibfk_2` FOREIGN KEY (`pid`) REFERENCES `student_practical` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `student_practical`
 --
 ALTER TABLE `student_practical`
-  ADD CONSTRAINT `student_practical_ibfk_1` FOREIGN KEY (`f11`) REFERENCES `faculty` (`id`),
-  ADD CONSTRAINT `student_practical_ibfk_2` FOREIGN KEY (`f12`) REFERENCES `faculty` (`id`),
-  ADD CONSTRAINT `student_practical_ibfk_3` FOREIGN KEY (`f21`) REFERENCES `faculty` (`id`),
-  ADD CONSTRAINT `student_practical_ibfk_4` FOREIGN KEY (`f22`) REFERENCES `faculty` (`id`),
-  ADD CONSTRAINT `student_practical_ibfk_5` FOREIGN KEY (`f31`) REFERENCES `faculty` (`id`),
-  ADD CONSTRAINT `student_practical_ibfk_6` FOREIGN KEY (`f32`) REFERENCES `faculty` (`id`),
-  ADD CONSTRAINT `student_practical_ibfk_7` FOREIGN KEY (`f41`) REFERENCES `faculty` (`id`),
-  ADD CONSTRAINT `student_practical_ibfk_8` FOREIGN KEY (`f42`) REFERENCES `faculty` (`id`);
+  ADD CONSTRAINT `student_practical_ibfk_1` FOREIGN KEY (`f11`) REFERENCES `faculty` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `student_practical_ibfk_2` FOREIGN KEY (`f12`) REFERENCES `faculty` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `student_practical_ibfk_3` FOREIGN KEY (`f21`) REFERENCES `faculty` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `student_practical_ibfk_4` FOREIGN KEY (`f22`) REFERENCES `faculty` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `student_practical_ibfk_5` FOREIGN KEY (`f31`) REFERENCES `faculty` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `student_practical_ibfk_6` FOREIGN KEY (`f32`) REFERENCES `faculty` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `student_practical_ibfk_7` FOREIGN KEY (`f41`) REFERENCES `faculty` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `student_practical_ibfk_8` FOREIGN KEY (`f42`) REFERENCES `faculty` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `student_theory`
